08191943689bc8c6609a4a3150387e52a2ef6dcd
Manuele Vancin <mfvancin@gmail.com>
2024-05-23 17:15:27 +0100

    MiniProjectDay4

--- server/app.js
+++ server/app.js
@@ -5,12 +5,10 @@ const PORT = 5005;
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
-// ...
-
+require("./db");
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
-
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
@@ -28,6 +26,12 @@ app.use(cookieParser());
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
+
+const cohortRoutes = require("./routes/cohort.route");
+app.use("/api", cohortRoutes);
+
+const studentsRoutes = require("./routes/students.routes");
+app.use("/api", studentsRoutes);
 
 
 // START SERVER
---
+++ server/routes/cohort.route.js
@@ -0,0 +1,87 @@
+const router = require('express').Router();
+
+const mongoose = require("mongoose");
+
+const Students = require("../models/Students.model");
+const Cohorts = require("../models/Cohort.model");
+
+router.get('/cohorts', (req, res) => {
+  Cohorts.find()
+    .then((cohorts) => res.json(allCohorts))
+    .catch((err) => {
+      console.log("Error while getting the cohorts", err);
+      res.status(500).json({ message: "Error while getting the cohorts" });
+  });
+});
+  
+router.post('/cohorts', (req, res, next) => {
+  const {
+    inProgress,
+    cohortSlug,
+    cohortName,
+    program,
+    format,
+    campus,
+    startDate,
+    endDate,
+    programManager,
+    leadTeacher,
+    totalHours
+  } = req.body;
+  
+  Cohorts.create({
+    inProgress,
+    cohortSlug,
+    cohortName,
+    program,
+    format,
+    campus,
+    startDate,
+    endDate,
+    programManager,
+    leadTeacher,
+    totalHours 
+  })
+    .then((cohort) => {
+        console.log("New cohort", cohort)
+        res.json(cohort)
+    })
+    .catch((err) => {
+      console.log("Error while creating the cohort", err);
+      res.status(500).json({ message: "Error while creating the cohort" });
+  });
+});
+  
+router.put('/cohorts/:cohortsId', (req, res) => {
+  const { cohortsId } = req.params;
+       
+  if (!mongoose.Types.ObjectId.isValid(cohortsId)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+       
+  Cohorts.findByIdAndUpdate(cohortsId, req.body, { new: true })
+    .then((updatedCohorts) => res.json(updatedCohorts))
+    .catch((err) => {
+      console.log("Error while updating the cohorts list", err);
+      res.status(500).json({ message: "Error while updating the cohorts list" });
+  });
+});
+
+router.delete('/cohorts/:cohortsId', (req, res, next) => {
+  const { cohortsId } = req.params;
+       
+  if (!mongoose.Types.ObjectId.isValid(cohortsId)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+       
+  Project.findByIdAndDelete(cohortsId)
+    .then(() => res.json({ message: `Cohort with ${cohortsId} is removed successfully.` }))
+    .catch((err) => {
+      console.log("Error while deleting the cohort", err);
+      res.status(500).json({ message: "Error while deleting the cohort" });
+  });
+});
+  
+module.exports = router;
